c75c19468cb45efaae00a477362c1346f99f09f5
hemant140 <hemo.hm576@gmail.com>
2025-03-25 10:22:19 +0530

    message api

--- services/message.service.js
+++ services/message.service.js
@@ -1,8 +1,9 @@
-export const getById = async (notificationId) => {
+import Message from "../models/message.model";
 
+export const getById = async (messageId) => {
     try {
-
-        const response = await
+        const response = await Message.findById({ _id: messageId }, { __v: 0 })
+        return response || null;
 
     } catch (error) {
         console.error("Something went wrong finding message by Id", error.message)
@@ -11,7 +12,31 @@ export const getById = async (notificationId) => {
 
 }
 
-export const getAllMessage = () => {
+export const getAllMessage = async (page, limit, search) => {
+    try {
+
+        const match = search ? {
+            $or: [
+                {
+
+                }
+            ]
+
+        } : {}
+
+        const response = await Message.aggregate([
+            {
+                $match: match
+            }
+        ])
+
+
+
+
+    } catch (error) {
+        console.error("Something went wrong getting all users", error.message)
+        throw error;
+    }
 
 }
 
